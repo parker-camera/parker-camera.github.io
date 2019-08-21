58a3717df4ea3cf601d0ddd3f4d2c7b642d8817b
Danny Parker <dcparker88@gmail.com>
2019-08-20 21:54:58 -0500

    Revert "adding a gemfile to the build"

--- test.sh
+++ test.sh
@@ -3,6 +3,5 @@
 docker build -t parker-camera .
 docker run \
   --volume="$PWD:/srv/jekyll" \
-  --volume="$PWD/vendor/bundle:/usr/local/bundle" \
   -it -p 4000:4000 parker-camera \
   jekyll serve
