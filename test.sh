5008177cbb948c55c597532efe846c41fe486d2e
dcparker88 <dcparker88@gmail.com>
2019-08-20 22:12:42 -0500

    adding proper gemfile and test

--- test.sh
+++ test.sh
@@ -6,5 +6,6 @@ export JEKYLL_VERSION=3.8
 echo "starting the container"
 docker run \
   --volume="$PWD:/srv/jekyll" \
+  --volume="$PWD/vendor/bundle:/usr/local/bundle" \
   -it -p 4000:4000 jekyll/jekyll:$JEKYLL_VERSION \
   jekyll serve
